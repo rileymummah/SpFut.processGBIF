6c08829621bb42b22a9f3244e38265756481deff
lanescher <clanescher@gmail.com>
2025-12-16 13:37:28 -0500

    clean up

--- R/processGBIF.R
+++ R/processGBIF.R
@@ -62,58 +62,7 @@ process_gbif <- function(scientificName,
                              user = user, pwd = pwd, email = email)
   
   
-  
-  
-  
-  # for (n in 1:length(scinames)) {
-  #   cat(scinames[n], "\n")
-  # 
-  #   gbif.raw1 <- download_gbif(scientificName = scinames[n],
-  #                              user = user, pwd = pwd, email = email)
-  # 
-  #   # Only keep entries that match scientific name
-  #   index <- which(gbif.raw1$dat$species == scinames[n] | stringr::str_to_sentence(gbif.raw1$dat$verbatimScientificName) == scinames[n])
-  #   gbif.raw1$dat <- gbif.raw1$dat[index,]
-  # 
-  #   # Add to previously downloaded data
-  #   if (nrow(gbif.raw1$dat) > 0) {
-  # 
-  #     # First, look for and remove duplicates
-  #     if (length(nrow(gbif.raw$dat)) > 0) {
-  #       old <- paste0(gbif.raw$dat$year, gbif.raw$dat$month, gbif.raw$dat$day, gbif.raw$dat$decimalLatitude, gbif.raw$dat$decimalLongitude)
-  #       new <- paste0(gbif.raw1$dat$year, gbif.raw1$dat$month, gbif.raw1$dat$day, gbif.raw1$dat$decimalLatitude, gbif.raw1$dat$decimalLongitude)
-  # 
-  #       rm <- which(new %in% old)
-  #       if (length(rm) > 0) {
-  #         gbif.raw1$dat <- gbif.raw1$dat[-rm,]
-  #         cat("Removing ", length(rm), " records that are likely duplicates\n")
-  #       }
-  # 
-  #       if (nrow(gbif.raw1$dat) == 0) {
-  #         cat("Adding 0 records with scientific name ", scinames[n], "\n")
-  #         next
-  #       }
-  # 
-  #     }
-  # 
-  #     gbif.raw1$dat$eventDate <- as.character(gbif.raw1$dat$eventDate)
-  # 
-  #     gbif.raw1$dat <- dplyr::select(gbif.raw1$dat, !c(.data$catalogNumber))
-  # 
-  #     # Then add to existing data
-  #     gbif.raw$dat <- dplyr::bind_rows(gbif.raw$dat, gbif.raw1$dat)
-  #     cat("Adding ", nrow(gbif.raw1$dat), " records with scientific name ", scinames[n], "\n")
-  # 
-  #     cit <- paste0(scinames[n], ": ", gbif.raw1$citation)
-  #     gbif.raw$citation <- c(gbif.raw$citation, cit)
-  # 
-  # 
-  #   }
-  # 
-  # }
-
-
-  if (length(gbif.raw$dat) == 0) {
+  if (nrow(gbif.raw$dat) == 0) {
     cat("There are no GBIF data for this species.\n")
 
 
@@ -178,7 +127,7 @@ process_gbif <- function(scientificName,
         writeLines(gbif.raw$citation, paste0(citation.path, "iNat-", sp.code, ".txt"))
       }
     } else {
-      cat("There are no iNaturalist records for this species")
+      cat("There are no iNaturalist records for this species\n")
     }
 
     if (nrow(mus) > 0 & "museum" %in% keep) {
@@ -225,70 +174,10 @@ process_gbif <- function(scientificName,
       }
 
     } else {
-      cat("There are no museum records for this species")
+      cat("There are no museum records for this species\n")
     }
 
-    rm(list=c('dat','gbif.clean','gbif.raw','gbif.raw1','inat','mus','index'))
-
-    # # clean
-    # gbif.clean <- clean_gbif(gbif.raw$dat,
-    #                          startYear,
-    #                          endYear,
-    #                          centroidBufferKm, capitalBufferKm, institutionBufferKm)
-
-    # # Keep records from desired source
-    # if (keep %in% c("iNat", "iNaturalist", "inat", "inaturalist")) {
-    #   gbif.clean <- dplyr::filter(gbif.clean, source == "iNat")
-    # } else if (keep %in% c("museum", "Museum")) {
-    #   gbif.clean <- dplyr::filter(gbif.clean, source == "Museum")
-    # } else {
-    #   cat("'keep' must be 'iNaturalist' or 'Museum'")
-    # }
-    #
-    # if (nrow(gbif.clean) > 0) {
-    #
-    #   # format for species futures
-    #   gbif.clean <- gbif.clean %>%
-    #     dplyr::mutate(date = substr(eventDate, 1, 10),
-    #                    date = lubridate::as_date(date),
-    #                    year = lubridate::year(date),
-    #                    lat = decimalLatitude,
-    #                    lon = decimalLongitude,
-    #                    coord.unc = coordinateUncertaintyInMeters,
-    #                    survey.conducted = 1,
-    #                    count = 1,
-    #                    data.type = "PO",
-    #                    age = "NR",
-    #                    individual.id = NA,
-    #                    time.to.detect = NA,
-    #                    species = sp.code) %>%
-    #
-    #     # make site.id
-    #     dplyr::group_by(lat, lon) %>%
-    #     dplyr::mutate(site.id = paste0(source, dplyr::cur_group_id())) %>%
-    #     dplyr::ungroup() %>%
-    #
-    #
-    #     # get survey.id
-    #     dplyr::mutate(survey.id = 1:nrow(.),
-    #                    pass.id = 1,
-    #                    survey.pass = paste0(survey.id, "_", pass.id)) %>%
-    #
-    #
-    #     # select cols to keep
-    #     dplyr::select(site.id, lat, lon, stateProvince, coord.unc, eventDate,
-    #                   day, month, year, survey.conducted, survey.id, pass.id,
-    #                   survey.pass, data.type, species, age, individual.id,
-    #                   time.to.detect, count)
-    #
-    #   all <- list(dat = gbif.clean,
-    #               citation = gbif.raw$citation)
-    #
-    #   return(all)
-
-    #} #else {
-      #cat("There are no data left after cleaning\n")
-    #}
+    rm(list=c('dat','gbif.clean','gbif.raw','inat','mus'))
 
 
   } # end there are iNat data for this species
